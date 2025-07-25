4bd2e55cdd7c25befa24877f071b7cd803108231
Akshata Potale <potaleakshata13@gmail.com>
2025-07-25 22:50:12 +0530

    Added a test case for registering an account with all fields left blank

--- src/test/java/tutorialsninja/register/TC_RF_004.java
+++ src/test/java/tutorialsninja/register/TC_RF_004.java
@@ -1,10 +1,49 @@
 package tutorialsninja.register;
 
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TC_RF_004 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@Test
+	public void verifyRegisteringAccountWithoutFillingDataInFields() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo");
+		
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
 
+		String expectedFirstNameWarning = "First Name must be between 1 and 32 characters!";
+		String expectedLastNameWarning = "Last Name must be between 1 and 32 characters!";
+		String expectedEmailWarning = "E-Mail Address does not appear to be valid!";
+		String expectedTelePhoneWarning= "Telephone must be between 3 and 32 characters!";
+		String expectedPasswordWarning = "Password must be between 4 and 20 characters!";
+		String expectedPrivacyPolicyWarning = "Warning: You must agree to the Privacy Policy!";
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-firstname']/following-sibling::div")).getText(), expectedFirstNameWarning);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-lastname']/following-sibling::div")).getText(), expectedLastNameWarning);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-email']/following-sibling::div")).getText(), expectedEmailWarning);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-telephone']/following-sibling::div")).getText(), expectedTelePhoneWarning);
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-password']/following-sibling::div")).getText(), expectedPasswordWarning);
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']")).getText(), expectedPrivacyPolicyWarning);
+
+		driver.quit();
+	}
+	
+	public  String generateNewEmail() {
+		return new Date().toString().replaceAll("\\s", "").replaceAll("\\:", "")+"@gmail.com";
 	}
 
 }
