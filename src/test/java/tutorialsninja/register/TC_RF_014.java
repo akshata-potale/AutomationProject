8abd2cbc7e06c014feaea1af47f4c7edc6a4cc93
Akshata Potale <potaleakshata13@gmail.com>
2025-08-02 12:26:02 +0530

    Added automation script for verify * symbol is present in all mandatory fields.

--- src/test/java/tutorialsninja/register/TC_RF_014.java
+++ src/test/java/tutorialsninja/register/TC_RF_014.java
@@ -8,14 +8,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class TC_RF_014 {
+	WebDriver driver;
+	
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
 	
 	@Test
 	public void verifyMandatoryFieldsSymbolAndColorInRegisterAccountPage() {
 		
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
 		driver.get("https://tutorialsninja.com/demo");
@@ -24,23 +31,65 @@ public class TC_RF_014 {
 		driver.findElement(By.linkText("Register")).click();
 		
 		WebElement firstNameLabel = driver.findElement(By.cssSelector("label[for=\"input-firstname\"]"));
-		String expectedFnContent = "\"* \"";
 		
+		String expectedContent = "\"* \"";
+		String expectedColor = "rgb(255, 0, 0)";
 		
-		String expectedFnColor = "rgb(255, 0, 0)";
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
 		String fnContent = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('content');", firstNameLabel);
-		System.out.println(fnContent);
-		
 		String fnColor = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('color');", firstNameLabel);
-		System.out.println(fnColor);
 		
-		Assert.assertEquals(fnContent, expectedFnContent);
-		Assert.assertEquals(fnColor, expectedFnColor);
+		Assert.assertEquals(fnContent, expectedContent);
+		Assert.assertEquals(fnColor, expectedColor);
 		
-		driver.quit();
-
-
+		
+		WebElement lastNameLabel = driver.findElement(By.cssSelector("label[for=\"input-lastname\"]"));
+		
+		String lnContent = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('content');", lastNameLabel);
+		String lnColor = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('color');", lastNameLabel);		
+		Assert.assertEquals(lnContent, expectedContent);
+		Assert.assertEquals(lnColor, expectedColor);
+		
+		
+		WebElement emailLabel = driver.findElement(By.cssSelector("label[for=\"input-email\"]"));
+		
+		String emailContent = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('content');", emailLabel);
+		String emailColor = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('color');", emailLabel);		
+		Assert.assertEquals(emailContent, expectedContent);
+		Assert.assertEquals(emailColor, expectedColor);
+		
+		
+		WebElement telephoneLabel = driver.findElement(By.cssSelector("label[for=\"input-telephone\"]"));
+		
+		String tpContent = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('content');", telephoneLabel);
+		String tpColor = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('color');", telephoneLabel);		
+		Assert.assertEquals(tpContent, expectedContent);
+		Assert.assertEquals(tpColor, expectedColor);
+		
+		
+		WebElement passwordLabel = driver.findElement(By.cssSelector("label[for=\"input-password\"]"));
+		
+		String psdContent = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('content');", passwordLabel);
+		String psdColor = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('color');", passwordLabel);		
+		Assert.assertEquals(psdContent, expectedContent);
+		Assert.assertEquals(psdColor, expectedColor);
+		
+		
+		WebElement passwordConfirmLabel = driver.findElement(By.cssSelector("label[for=\"input-confirm\"]"));
+		
+		String psdConfirmContent = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('content');", passwordConfirmLabel);
+		String psdConfirmColor = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('color');", passwordConfirmLabel);		
+		Assert.assertEquals(psdConfirmContent, expectedContent);
+		Assert.assertEquals(psdConfirmColor, expectedColor);
+		
+		//*
+		
+		WebElement privacyPolicyLabel = driver.findElement(By.cssSelector("[class='pull-right']"));
+		
+		String privacyPolicyContent = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('content');", privacyPolicyLabel);
+		String privacyPolicyColor = (String)jse.executeScript("return window.getComputedStyle(arguments[0], '::before').getPropertyValue('color');", privacyPolicyLabel);		
+		Assert.assertEquals(privacyPolicyContent, expectedContent);
+		Assert.assertEquals(privacyPolicyColor, expectedColor);
 		
 		
 	}
