ee72f41ef373fd5f41531bd0ae94e8ddbb5b778c
Akshata Potale <potaleakshata13@gmail.com>
2025-08-03 20:59:04 +0530

    Automated testcase to check password complexity

---
+++ src/test/java/tutorialsninja/register/TC_RF_017.java
@@ -0,0 +1,56 @@
+package tutorialsninja.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import utils.CommonUtils;
+
+public class TC_RF_017 {
+	WebDriver driver;
+	
+	@AfterMethod
+	public void teardown() {
+		driver.quit();
+	}
+	
+	
+	@Test (dataProvider="PasswordProvider")
+	public void verifyRegisteringAccountAndCheckingPasswordComplexityStandards(String passwordText) {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo");
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		
+		driver.findElement(By.id("input-firstname")).sendKeys("Akshata");
+		driver.findElement(By.id("input-lastname")).sendKeys("Potale");
+		driver.findElement(By.id("input-email")).sendKeys(CommonUtils.generateNewEmail());
+		driver.findElement(By.id("input-telephone")).sendKeys("9878776767");
+		driver.findElement(By.id("input-password")).sendKeys(passwordText);
+		driver.findElement(By.id("input-confirm")).sendKeys(passwordText);
+		driver.findElement(By.xpath("//input[@name='newsletter'][@value='1']")).click();
+		driver.findElement(By.name("agree")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		
+		String warningMessage = "Password entered doesnot match with Complexity Standards";
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id=\"input-password\"]/following-sibling::div")).getText(), warningMessage);
+		Assert.assertFalse(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Success']")).isDisplayed());
+	}
+	
+	@DataProvider(name = "PasswordProvider")
+	public Object[][] supplyPasswords() {
+		
+		Object[][] data = {{"12345"},{"abcdefghi"},{"abcd1234"},{"abcde123#"},{"ABCDE123@"}};
+		return data;
+		
+	}
+
+}
