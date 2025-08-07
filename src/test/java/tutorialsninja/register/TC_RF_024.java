35b55b4c4e60d5bbc660961c7f6e5813786e44f3
Akshata Potale <potaleakshata13@gmail.com>
2025-08-07 11:40:29 +0530

    Automated testcase to verify registering account without entering password in confirm password field

---
+++ src/test/java/tutorialsninja/register/TC_RF_024.java
@@ -0,0 +1,44 @@
+package tutorialsninja.register;
+
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import utils.CommonUtils;
+
+public class TC_RF_024 {
+	
+	@Test
+	public void verifyRegisteringAccountWithoutEnteringPasswordIntoPasswordConfirmField() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://tutorialsninja.com/demo");
+		
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+
+		driver.findElement(By.id("input-firstname")).sendKeys("Akshata");
+		driver.findElement(By.id("input-lastname")).sendKeys("Potale");
+		driver.findElement(By.id("input-email")).sendKeys(CommonUtils.generateNewEmail());
+		driver.findElement(By.id("input-telephone")).sendKeys("878676766");
+		driver.findElement(By.id("input-password")).sendKeys("Akshata@123");
+		driver.findElement(By.xpath("//input[@name='newsletter'][@value='1']")).click();
+		driver.findElement(By.name("agree")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		
+		String expectedWarning = "Password confirmation does not match password!";
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//input[@id='input-confirm']/following-sibling::div")).getText(), expectedWarning);
+
+		driver.quit();
+	}
+
+}
